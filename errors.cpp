9007eb111c43462550896ab9503e8aa3fd33835b
Thomas Bertozzi <tbertozz@F3C029.42adel.org.au>
2023-03-30 13:54:03 +1030

    Adding error codes for future use

---
+++ errors.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbertozz <tbertozz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/30 13:24:57 by tbertozz          #+#    #+#             */
+/*   Updated: 2023/03/30 13:28:15 by tbertozz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef errors_hpp
+#define errors_hpp
+
+#define	USERALREADYJOINED 0
+#define USERISJOINED 1
+#define NOTINCHANNEL 2
+#define BANNEDFROMCHANNEL 3
+#define TOOMANYCHANNELS 4
+#define BADCHANNELKEY 5
+#define CHANNELISFULL 6
+#define NOSUCHCHANNEL 7
+#define USERISBANNED 8
+#define BADCHANNELMASK 9
+#define USERNOTINCHANNEL -1
+#define USERNOTFOUND -1
+
+#include "server.hpp"
+
+
+
+#endif
