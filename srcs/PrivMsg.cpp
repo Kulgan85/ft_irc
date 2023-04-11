7cd315cb095642c82f93dcab13595a4d0737f998
Blake Morrison <sapan@pop-os.localdomain>
2023-04-11 11:28:02 +0930

    Added comma seperator function for private message targets

--- srcs/PrivMsg.cpp
+++ srcs/PrivMsg.cpp
@@ -1,7 +1,24 @@
-#include "Server.hpp"
+#include "../includes/Server.hpp"
+
+static std::vector<std::string>	getTargets(std::string commaList)
+{
+	std::vector<std::string> output;
+	std::string::size_type prevPos = 0, pos = 0;
+
+	while ((pos = commaList.find(',', pos)) != std::string::npos)
+	{
+		std::string subStr(commaList.substr(prevPos, pos - prevPos));
+		output.push_back(subStr);
+		prevPos = ++pos;
+	}
+	output.push_back(commaList.substr(prevPos, pos - prevPos));
+	return output;
+}
 
 void	Server::PMSG(const int &sender_fd)
 {
 	std::vector<std::string>	args = Server::_splitString(this->_clients[sender_fd]->getMessage());
+	std::vector<std::string>	targets = getTargets(args[1]);
+
 	
 }
