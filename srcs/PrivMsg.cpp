dce769e322b0083868e88f3174119839aa08f19e
Blake Morrison <sapan@pop-os.localdomain>
2023-04-11 13:20:56 +0930

    Completed user private messages

--- srcs/PrivMsg.cpp
+++ srcs/PrivMsg.cpp
@@ -20,5 +20,36 @@ void	Server::PMSG(const int &sender_fd)
 	std::vector<std::string>	args = Server::_splitString(this->_clients[sender_fd]->getMessage());
 	std::vector<std::string>	targets = getTargets(args[1]);
 
-	
+	for (int i = 0; i < targets.size(); i++)
+	{
+		if (targets[i][0] == '#' || targets[i][0] == '&')
+		{
+			// TODO: Channel
+		}
+		else
+		{
+			std::map<int, Client*>::iterator it;
+			for (it = _clients.begin(); it != _clients.end(); it++)
+			{
+				if ((*it).second->getNickname() == targets[i])
+				{
+					std::string toSend = ":";
+					toSend.append(_clients.find(sender_fd)->second->getNickname());
+					toSend.append(" PRIVMSG ");
+					toSend.append((*it).second->getNickname());
+					toSend.append(args[2]);
+					toSend.append("\r\n");
+					send((*it).second->getFd(), toSend.c_str(), toSend.size(), MSG_DONTWAIT);
+					break;
+				}
+			}
+			if (it == _clients.end())
+			{
+				std::string toSend = ":ircserv 401 ";
+				toSend.append(_clients.find(sender_fd)->second->getNickname());
+				toSend.append(" :No such nick\r\n");
+				send(sender_fd, toSend.c_str(), toSend.size(), MSG_DONTWAIT);
+			}
+		}
+	}
 }
