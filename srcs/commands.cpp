53fb43f75efef65b02e32847c65b7cf15ec1bfce
Thomas Bertozzi <tbertozz@F3C024.42adel.org.au>
2023-04-13 12:32:31 +0930

    Updated JOIN command

--- srcs/commands.cpp
+++ srcs/commands.cpp
@@ -52,6 +52,70 @@ std::vector<std::string> Server::_splitString(std::string str)
 	return (tokens);
 }
 
+struct Channel {
+    std::string name;
+    std::vector<int> clients;
+};
+
+void	Server::JOIN(const int &sender_fd)
+{
+	Channel channel;
+	std::vector<std::string> channel_name = this->_splitString(this->_clients[sender_fd]->getMessage);
+	if (channel_name.empty() == true)
+		return ;
+	if (channel_name.front != '#' || channel_name.front != '&')
+	{
+		std::cout << "Error: Channel name must start with a # or an &" << std::endl;
+		return ; 
+	}
+	for (const auto& channel : channels)
+	{
+		if (channel.name == channel_name) 
+		{
+			join_channel(sender_fd, channel_name);
+		}
+		
+	}
+}
+
+
+
+std::vector<Channel> channels;
+
+// Create a new channel with the given name
+void create_channel(std::string name) {
+    Channel channel;
+    channel.name = name;
+    channels.push_back(channel);
+}
+
+// Add a client to a channel
+void join_channel(int client_fd, std::string channel_name) {
+    for (auto& channel : channels) {
+        if (channel.name == channel_name) {
+            channel.clients.push_back(client_fd);
+            std::cout << "Client " << client_fd << " joined channel " << channel_name << "\n";
+            return;
+        }
+    }
+    std::cout << "Channel " << channel_name << " does not exist\n";
+}
+
+// Remove a client from a channel
+void leave_channel(int client_fd, std::string channel_name) {
+    for (auto& channel : channels) {
+        if (channel.name == channel_name) {
+            auto it = std::find(channel.clients.begin(), channel.clients.end(), client_fd);
+            if (it != channel.clients.end()) {
+                channel.clients.erase(it);
+                std::cout << "Client " << client_fd << " left channel " << channel_name << "\n";
+                return;
+            }
+        }
+    }
+    std::cout << "Client " << client_fd << " is not in channel " << channel_name << "\n";
+}
+
 // void	Server::JOIN(const int &sender_fd) {(void)sender_fd;}
 // 	std::unordered_map<std::string, /*Nickname??*/ channels;
 // 	std::string channel_name = message.substr(5, message.find("\r\n") - 5);
