f3cb690a55cac47408bb01e2ca5825d2b0d41d4e
Thomas Bertozzi <tbertozz@F3C024.42adel.org.au>
2023-04-14 15:50:09 +0930

    Join and Leave good

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -7,7 +7,9 @@ class Channel
 {
 	public:
 	int	JoinChannel(Client* toJoin);
+	int	JoinChannel(int sender_fd);		//TO DO - ANNOUNCE USER JOINING
 	int	LeaveChannel(Client* toLeave);
+	int	LeaveChannel(int sender_fd, std::string reason);	//TO DO - ANNOUNCE USER LEAVING & REASON
 
 	private:
 	std::string 			_name;
