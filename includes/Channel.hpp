406d45d8fcd43fc677d558c150f51fe58772e2c4
Blake Morrison <sapan@pop-os.localdomain>
2023-04-14 15:09:09 +0930

    Added channel constructors

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -6,12 +6,19 @@
 class Channel
 {
 	public:
+	Channel(std::string name);
+	~Channel();
 	int	JoinChannel(Client* toJoin);
 	int	LeaveChannel(Client* toLeave);
+	int	KickClient(Client* toKick);
+	int	ChangeTopic(std::string newTopic);
 
 	private:
-	std::string 			_name;
+	Channel();
+
+	const std::string 		_name;
 	std::vector<Client*>	_clients;
+	std::string				_topic;
 	std::vector<std::string>	_operatorIPs;
 	std::vector<std::string>	_bannedIPs;
 
